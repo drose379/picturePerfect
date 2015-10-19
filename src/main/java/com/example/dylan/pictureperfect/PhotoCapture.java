7ff34a2e57a9bec46d430e439eb0216d21b4117d
Dylan Rose <dylanrose60@gmail.com>
2015-10-19 07:45:18 -0400

    Working on PhotoCapture with PhotoCaptureBuilder

--- src/main/java/com/example/dylan/pictureperfect/PhotoCapture.java
+++ src/main/java/com/example/dylan/pictureperfect/PhotoCapture.java
@@ -1,19 +1,33 @@
 package com.example.dylan.pictureperfect;
 
+import android.content.Context;
+
 /**
  * Created by dylan on 10/19/15.
  */
 public class PhotoCapture {
 
+    public interface PhotoCaptureCallback {
+        void getPhotoResult( PhotoResult result );
+    }
+
+    public PhotoCapture( Context context ) {
+        //cast context to Callback
+    }
+
     private String from = PhotoCaptureBuilder.PICKER;
+    private boolean crop = true;
 
     public void setFrom( String source ) {
         from = source;
     }
+    public void setCrop( boolean crop ) {
+        this.crop = crop;
+    }
 
 
     public void start() {
-
+        //make sure source is one of the Builder's constants
     }
 
 
@@ -23,27 +37,27 @@ public class PhotoCapture {
         public static final String CAMERA = "CAMERA";
         public static final String PICKER = "PICKER";
 
+        private Context context;
         private PhotoCapture photoCapture;
 
+        public PhotoCaptureBuilder( Context context ) {
+            this.context = context;
+        }
+
         public PhotoCaptureBuilder from( String photoSource ) {
-
-            if ( photoCapture != null ) {
-                //set the from field
-            } else {
-
-            }
-
+            photoCapture = photoCapture == null ? new PhotoCapture( context ) : photoCapture;
+            photoCapture.setFrom( photoSource );
             return this;
         }
 
         public PhotoCaptureBuilder crop( boolean crop ) {
-
+            photoCapture = photoCapture == null ? new PhotoCapture( context ) : photoCapture;
+            photoCapture.setCrop( crop );
             return this;
         }
 
         public PhotoCapture build() {
-
-            
+            return photoCapture;
         }
 
 
---
+++ src/main/java/com/example/dylan/pictureperfect/PhotoResult.java
@@ -0,0 +1,8 @@
+package com.example.dylan.pictureperfect;
+
+/**
+ * Created by dylan on 10/19/15.
+ */
+public class PhotoResult {
+    //POJO representing the result of an image taken / grabbed with PhotoCapture
+}
